de46c628ae44d289d995988ff2d5c2a35ff4a94e
yu-ki <triple321jhango@gmail.com>
2023-11-17 06:31:01 +0900

    alb create done

---
+++ elb.tf
@@ -0,0 +1,15 @@
+#------------------------------------
+# ALB
+#------------------------------------
+resource "aws_lb" "cloudtech_alb" {
+  name               = "${var.project}-web-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups = [
+    aws_serurity_group.aws_security_group.cloudtech_web_sg.id
+  ]
+  subnets = [
+    aws_subnet.cloudtech_subnet_public1.id,
+    aws_subnet.cloudtech_subnet_public2.id
+  ]
+}
