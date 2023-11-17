bac4ac81f2d897abe09181d0155dcca7cb55eb8a
yu-ki <triple321jhango@gmail.com>
2023-11-17 10:01:08 +0900

    alb listener add

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,12 @@
 #------------------------------------
 # EC2
 #------------------------------------
-#最新のAMI IDを取得
+# 最新のAMI IDを取得
 data "aws_ssm_parameter" "amzn2_ami" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
-
-resource "aws_instance" "cloudtech_testserver_ec2" {
+# 1台目のEC2
+resource "aws_instance" "cloudtech_webserver_ec2_1" {
   ami                         = data.aws_ssm_parameter.amzn2_ami.value
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.cloudtech_subnet_private1.id
@@ -16,7 +16,23 @@ resource "aws_instance" "cloudtech_testserver_ec2" {
     aws_security_group.cloudtech_ec2_ssm_sg.id
   ]
   tags = {
-    Name    = "${var.project}-testserver"
+    Name    = "${var.project}-webserver-1"
+    Project = var.project
+  }
+}
+
+# 2台目のEC2
+resource "aws_instance" "cloudtech_webserver_ec2_2" {
+  ami                         = data.aws_ssm_parameter.amzn2_ami.value
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.cloudtech_subnet_private1.id
+  associate_public_ip_address = false
+  iam_instance_profile        = aws_iam_instance_profile.ssm_instance_profile.name
+  vpc_security_group_ids = [
+    aws_security_group.cloudtech_ec2_ssm_sg.id
+  ]
+  tags = {
+    Name    = "${var.project}-webserver-2"
     Project = var.project
   }
 }
--- elb.tf
+++ elb.tf
@@ -13,3 +13,52 @@ resource "aws_lb" "cloudtech_alb" {
     aws_subnet.cloudtech_subnet_public2.id
   ]
 }
+
+# リスナーの設定
+# 特定のポートでリクエストを待ち受け -> リクエストに基づいてトラフィックを処理
+# ALBのリスナーがHTTPトラフィックを処理する場合はport:80, HTTPSの場合はport:443でリクエストを待ち受ける
+resource "aws_lb_listener" "aws_lb_listener_http" {
+  load_balancer_arn = aws_lb.cloudtech_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # 受け取ったリクエストを指定されたターゲットグループへフォワード (転送)する
+  # type =forwardがその部分
+  # どこに転送するのか -> target groupで指定した場所へ
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.aws_lb_target_group.arn
+  }
+}
+
+#------------------------------------
+# ALB Target Group
+#------------------------------------
+resource "aws_lb_target_group" "aws_lb_target_group" {
+  name     = "${var.project}-web-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.cloudtech_vpc.id
+
+  tags = {
+    Name    = "${var.project}-web-tg"
+    Project = var.project
+  }
+}
+
+# ターゲットグループに所属させるインスタンスの登録
+# 1台目のEC2
+resource "aws_lb_target_group_attachment" "target_instance_1" {
+  target_group_arn = aws_lb_target_group.aws_lb_target_group.arn
+  target_id        = aws_instance.cloudtech_webserver_ec2_1.id
+}
+
+# 2台目のEC2
+resource "aws_lb_target_group_attachment" "target_instance_2" {
+  target_group_arn = aws_lb_target_group.aws_lb_target_group.arn
+  target_id        = aws_instance.cloudtech_webserver_ec2_2.id
+}
+
+#------------------------------------
+# ALB Target Group
+#------------------------------------
